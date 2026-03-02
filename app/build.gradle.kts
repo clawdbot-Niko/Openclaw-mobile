e8c9372642308604ee4f0e7420f4450a182d45c4
root <root@localhost.localdomain>
2026-03-02 22:44:42 +0000

    fix(android): use MaterialComponents app theme for resource linking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,6 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview:1.7.2")
   implementation("androidx.compose.material3:material3:1.3.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+  implementation("com.google.android.material:material:1.12.0")
   debugImplementation("androidx.compose.ui:ui-tooling:1.7.2")
 }
